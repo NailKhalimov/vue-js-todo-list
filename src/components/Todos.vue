d650ae173724c1618b81bfe9a9f65c29b241f276
NailHalimov <nail.halimov.rus@gmail.com>
2019-10-19 13:40:52 +0300

    added main files

---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <ToDoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  components: {
+    ToDoItem
+  },
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+</style>
